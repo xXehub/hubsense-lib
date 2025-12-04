4bd2fd13c131deac68522bd2d56b2b2d59c2b752
xXehub <artixsanni@gmail.com>
2025-12-04 20:49:20 +0700

    asdasd

--- ondevs/LinoriaLib.lua
+++ ondevs/LinoriaLib.lua
@@ -479,17 +479,6 @@ do
 
         ColorPicker:SetHSVFromRGB(ColorPicker.Value);
 
-        -- If this color picker is attached to a toggle, switch to absolute right alignment
-        local IsToggle = (ToggleLabel.Parent ~= Container)
-        if IsToggle then
-            -- Remove UIListLayout to allow absolute positioning (only for toggle labels)
-            for _, child in ipairs(ToggleLabel:GetChildren()) do
-                if child:IsA('UIListLayout') then
-                    child:Destroy()
-                end
-            end
-        end
-
         local DisplayFrame = Library:Create('Frame', {
             BackgroundColor3 = ColorPicker.Value;
             BorderColor3 = Library:GetDarkerColor(ColorPicker.Value);
@@ -499,12 +488,6 @@ do
             ZIndex = 6;
             Parent = ToggleLabel;
         });
-
-        if IsToggle then
-            -- Anchor to right-middle of the toggle label row
-            DisplayFrame.AnchorPoint = Vector2.new(1, 0.5)
-            DisplayFrame.Position = UDim2.new(1, -4, 0.5, 0)
-        end
 
         -- Transparency image taken from https://github.com/matas3535/SplixPrivateDrawingLibrary/blob/main/Library.lua cus i'm lazy
         local CheckerFrame = Library:Create('ImageLabel', {
@@ -1920,14 +1903,22 @@ do
         });
 
         local ToggleLabel = Library:CreateLabel({
-            Size = UDim2.new(1, -24, 1, 0); -- fill row, leave space for right padding
-            Position = UDim2.new(0, 20, 0, 0); -- offset right of checkbox
+            Size = UDim2.new(0, 216, 1, 0);
+            Position = UDim2.new(0, 20, 0, 0);
             TextSize = 14;
             Text = Info.Text;
             TextXAlignment = Enum.TextXAlignment.Left;
             ZIndex = 6;
             Parent = ToggleInner;
         });
+
+        local function UpdateLabelWidth()
+            local width = math.max(0, Container.AbsoluteSize.X - 32);
+            ToggleLabel.Size = UDim2.new(0, width, 1, 0);
+        end
+
+        UpdateLabelWidth()
+        Library:GiveSignal(Container:GetPropertyChangedSignal('AbsoluteSize'):Connect(UpdateLabelWidth))
 
         Library:Create('UIListLayout', {
             Padding = UDim.new(0, 4);
